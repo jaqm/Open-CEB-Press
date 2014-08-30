e0aeb2429a70e22c0382144e3c9316b14028742d
Jose Antonio Quevedo Muñoz <joseantonio.quevedo@gmail.com>
2014-08-30 08:47:01 +0200

    * moveCylinderUntilHighPressure(): Adding output information to the DEBUG_VERBOSE_MODE.

--- Firmware.ino
+++ Firmware.ino
@@ -370,7 +370,7 @@ void releasePressureManualMode(uint8_t digitalInputs[]){
 void moveCylinderUntilHighPressure(int cylinderPin, boolean &hpf){
 
   if (DEBUG_MODE){
-    Serial.println("moveCylinderUntilHighPressure: ");
+    Serial.print("moveCylinderUntilHighPressure: ");
     Serial.print("CylinderPin: "); Serial.println(cylinderPin);
   }
 
@@ -378,7 +378,11 @@ void moveCylinderUntilHighPressure(int cylinderPin, boolean &hpf){
   if(pinDigitalValueIs(PIN_PRESSURE,VALUE_HP_READ_DELAY)==VALUE_HP_ENABLED){
     hpf=true;
     digitalWrite(cylinderPin,VALUE_SOL_DISABLED);
+    if (DEBUG_MODE){Serial.print("moveCylinderUntilHighPressure: DONE");}
+    
+    if (DEBUG_VERBOSE_MODE){Serial.println("moveCylinderUntilHighPressure: Releasing pressure..");}
     releasePressure(cylinderPin,hpf);
+    if (DEBUG_VERBOSE_MODE){Serial.println("moveCylinderUntilHighPressure: Release pressure DONE");}
   }
 }
 
